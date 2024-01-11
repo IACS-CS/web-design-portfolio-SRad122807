ac2730f46d88db44513ecb8d62268e8d37a00fef
SRad122807 <154357170+SRad122807@users.noreply.github.com>
2024-01-11 17:23:14 +0000

    All thing

--- public/main.js
+++ public/main.js
@@ -1,1 +1,13 @@
 console.log('main.js loaded successfully!')
+
+const tabs = document.querySelectorAll(".sample-work .Work span");
+const activeTab = document.querySelector(".sample-work .Work .CYOA");
+
+tabs.forEach((tab) => {
+  tab.addEventListener("click", () => {
+    tabs.forEach((tab) => tab.classList.remove("active"));
+    tab.classList.add("active");
+  });
+});
+
+activeTab.classList.add("active");
